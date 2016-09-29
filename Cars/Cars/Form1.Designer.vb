1a680e4fa555418935727abf1227139c3a4d34dd
bb118314 <roflwhy@gmail.com>
2016-09-29 10:24:07 -0500

    Add files via upload

---
+++ Cars/Cars/Form1.Designer.vb
@@ -0,0 +1,180 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.picPur = New System.Windows.Forms.PictureBox()
+        Me.picPin = New System.Windows.Forms.PictureBox()
+        Me.picRed = New System.Windows.Forms.PictureBox()
+        Me.picGre = New System.Windows.Forms.PictureBox()
+        Me.picBro = New System.Windows.Forms.PictureBox()
+        Me.PictureBox7 = New System.Windows.Forms.PictureBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picPur, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picPin, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picRed, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picGre, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picBro, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 1)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(658, 288)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(156, 295)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Blast"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'picPur
+        '
+        Me.picPur.Image = CType(resources.GetObject("picPur.Image"), System.Drawing.Image)
+        Me.picPur.Location = New System.Drawing.Point(12, 12)
+        Me.picPur.Name = "picPur"
+        Me.picPur.Size = New System.Drawing.Size(103, 51)
+        Me.picPur.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picPur.TabIndex = 2
+        Me.picPur.TabStop = False
+        '
+        'picPin
+        '
+        Me.picPin.Image = CType(resources.GetObject("picPin.Image"), System.Drawing.Image)
+        Me.picPin.Location = New System.Drawing.Point(15, 69)
+        Me.picPin.Name = "picPin"
+        Me.picPin.Size = New System.Drawing.Size(100, 50)
+        Me.picPin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picPin.TabIndex = 3
+        Me.picPin.TabStop = False
+        '
+        'picRed
+        '
+        Me.picRed.Image = CType(resources.GetObject("picRed.Image"), System.Drawing.Image)
+        Me.picRed.Location = New System.Drawing.Point(15, 237)
+        Me.picRed.Name = "picRed"
+        Me.picRed.Size = New System.Drawing.Size(100, 50)
+        Me.picRed.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picRed.TabIndex = 4
+        Me.picRed.TabStop = False
+        '
+        'picGre
+        '
+        Me.picGre.Image = CType(resources.GetObject("picGre.Image"), System.Drawing.Image)
+        Me.picGre.Location = New System.Drawing.Point(15, 181)
+        Me.picGre.Name = "picGre"
+        Me.picGre.Size = New System.Drawing.Size(100, 50)
+        Me.picGre.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picGre.TabIndex = 5
+        Me.picGre.TabStop = False
+        '
+        'picBro
+        '
+        Me.picBro.Image = CType(resources.GetObject("picBro.Image"), System.Drawing.Image)
+        Me.picBro.Location = New System.Drawing.Point(15, 125)
+        Me.picBro.Name = "picBro"
+        Me.picBro.Size = New System.Drawing.Size(100, 50)
+        Me.picBro.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picBro.TabIndex = 6
+        Me.picBro.TabStop = False
+        '
+        'PictureBox7
+        '
+        Me.PictureBox7.Image = CType(resources.GetObject("PictureBox7.Image"), System.Drawing.Image)
+        Me.PictureBox7.Location = New System.Drawing.Point(558, 1)
+        Me.PictureBox7.Name = "PictureBox7"
+        Me.PictureBox7.Size = New System.Drawing.Size(100, 288)
+        Me.PictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox7.TabIndex = 7
+        Me.PictureBox7.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(275, 295)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "Reset"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(401, 295)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "Exit"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(656, 343)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.picBro)
+        Me.Controls.Add(Me.picGre)
+        Me.Controls.Add(Me.picRed)
+        Me.Controls.Add(Me.picPin)
+        Me.Controls.Add(Me.picPur)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox7)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Car"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picPur, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picPin, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picRed, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picGre, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picBro, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents picPur As System.Windows.Forms.PictureBox
+    Friend WithEvents picPin As System.Windows.Forms.PictureBox
+    Friend WithEvents picRed As System.Windows.Forms.PictureBox
+    Friend WithEvents picGre As System.Windows.Forms.PictureBox
+    Friend WithEvents picBro As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox7 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+
+End Class
---
+++ Cars/Cars/Form1.vb
@@ -0,0 +1,47 @@
+﻿Public Class Form1
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim intCount As Integer
+        Dim rand As New Random            'Dimming like a god
+        Const dblFinish As Double = 501
+
+
+        picPur.Left = rand.Next(625) + 15   '0XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX0
+        picPin.Left = rand.Next(625) + 15   '0 Sets the cars to a random integer between 0 and 625. 0
+        picRed.Left = rand.Next(625) + 15   '0XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX0
+        picGre.Left = rand.Next(625) + 15
+        picBro.Left = rand.Next(625) + 15
+
+
+        If picPur.Left >= dblFinish Then
+            MessageBox.Show("Purple is victorious!")
+        ElseIf picBro.Left >= dblFinish Then
+            MessageBox.Show("Brown takes the crown!")   'zZZZZZZZZZZZZZZZz
+        ElseIf picPin.Left >= dblFinish Then            'z Shows who won z
+            MessageBox.Show("Pink wins the race!")      'zZZZZZZZZZZZZZZZz
+        ElseIf picRed.Left >= dblFinish Then
+            MessageBox.Show("Red races ahead!")
+        ElseIf picGre.Left >= dblFinish Then
+            MessageBox.Show("Green finishes grand!")
+        End If
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Application.Exit() 'This doesn't need an explaination
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        picBro.Left = 20
+        picRed.Left = 20
+        picGre.Left = 20
+        picPin.Left = 20
+        picPur.Left = 20
+
+
+    End Sub
+End Class
